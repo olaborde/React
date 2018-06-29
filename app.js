f294f5c640895666e1d848562f660ecfd630ef16
olaborde <oml007@live.com>
2018-06-29 13:57:05 -0400

    makeDoggy function added

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+const kitty = (
+	<img 
+		src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" onClick={makeDoggy} />
+);
+
 // Write code here:
 const myList = (
   <ul>
@@ -18,5 +31,6 @@ const myList = (
 
 ReactDOM.render(
   myList,
+  kitty,
   document.getElementById('app')
 );
